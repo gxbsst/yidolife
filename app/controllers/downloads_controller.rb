e61ad2f41817fe350028b9308f2292335e270dd7
xiaofei li <xiaofei.li@sidways.com>
2012-12-14 17:56:34 +0800

    new

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 class DownloadsController < ApplicationController
-  # before_filter :authenticate
-  http_basic_authenticate_with :name => "media.yido", :password => "yidolife", :realm => "Yido"
+  # before_filter :authenticate 
 
   def index 
     @files =  Refinery::Resource.where("category = '指南'")  
